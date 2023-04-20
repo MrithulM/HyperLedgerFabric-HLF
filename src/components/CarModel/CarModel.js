ae01a45694e1b40eeee3d14d0bdd5325ec8befec
admin-ubuntu <test@ubuntu>
2023-04-20 10:55:16 +0000

    Added logo to the fabric

--- src/components/CarModel/CarModel.js
+++ src/components/CarModel/CarModel.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./CarModel.css";
 export default function CarModel(props) {
   const closeCarModel = () => {
     props.carModalClose();
   };
-  const { id, img, docType, color, make, model, owner } = props.clickedCar;
+  const { id, logo, img, docType, color, make, model, owner } =
+    props.clickedCar;
   return (
     <div className="car-model-bd">
       <div className="car-model-holder">
@@ -14,19 +15,25 @@ export default function CarModel(props) {
           </div>
           <div className="car-model-details-container">
             <div>
-              <div
-                // className="color-bar"
-                style={{
-                  background:
-                    color.toLowerCase() !== "white" ? `${color}` : "#D4D4D4",
-                  filter: "saturate(0.7)",
-                  width: "100%",
-                  height: "20px",
-                  transform: "translateX(-500px)",
-                  WebkitAnimation: "color-bar-slide 500ms 100ms forwards",
-                  zIndex: "2",
-                }}
-              ></div>
+              <div className="model-header">
+                <div
+                  // className="color-bar"
+                  style={{
+                    background:
+                      color.toLowerCase() !== "white" ? `${color}` : "#D4D4D4",
+                    filter: "saturate(0.7)",
+                    width: "100%",
+                    height: "60px",
+                    transform: "translateX(-500px)",
+                    WebkitAnimation: "color-bar-slide 500ms 100ms forwards",
+                    zIndex: "2",
+                    position: "relative",
+                  }}
+                >
+                  <div className="ribbon-end"></div>
+                </div>
+                <img className="logo" src={logo} alt="" />
+              </div>
               <div>
                 <p>Color: {color}</p>
                 <p>Type: {docType}</p>
