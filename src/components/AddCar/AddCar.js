c00bcf387717a96b4577306b85e41b820903e426
admin-ubuntu <test@ubuntu>
2023-04-21 14:47:22 +0530

    Added delete car functionality

--- src/components/AddCar/AddCar.js
+++ src/components/AddCar/AddCar.js
@@ -3,7 +3,8 @@ import "./AddCar.css";
 import axios from "axios";
 import FileBase64 from "react-file-base64";
 export default function AddCar(props) {
-  const [imgB64, setImgB64] = useState(null);
+  const [logoB64, setLogoB64] = useState("");
+  const [imgB64, setImgB64] = useState("");
   const idRef = useRef(null);
   const modelRef = useRef(null);
   const nameRef = useRef(null);
@@ -30,6 +31,7 @@ export default function AddCar(props) {
     const colour = colorRef.current.value;
     const owner = ownerRef.current.value;
     const details = {
+      logo: logoB64,
       img: imgB64,
       carid,
       make: model,
@@ -41,6 +43,7 @@ export default function AddCar(props) {
       await axios.post("http://4.246.223.78:8080/api/addcar", details);
       console.log("Added car!");
       alert("Added car!");
+      setLogoB64("");
       setImgB64("");
       idRef.current.value = "";
       modelRef.current.value = "";
@@ -61,12 +64,65 @@ export default function AddCar(props) {
       >
         <p>Add a Car</p>
         <div className="input-properties">
+          <label for="carimg">Logo:</label>
+          <FileBase64
+            type="file"
+            multiple={false}
+            max-size="1500"
+            onDone={({ base64 }) => {
+              let padding;
+              let inBytes;
+              let base64Length;
+              if (base64.endsWith("==")) {
+                padding = 2;
+              } else if (base64.endsWith("=")) {
+                padding = 1;
+              } else {
+                padding = 0;
+              }
+
+              base64Length = base64.length;
+              console.log(base64Length);
+              inBytes = (base64Length / 4) * 3 - padding;
+              let kb = inBytes / 1000;
+              let mb = kb / 1000;
+              if (mb < 1) {
+                setLogoB64(base64);
+              } else {
+                alert("File size too high! Size limit: 1.5MB");
+                closeModalHandler();
+              }
+            }}
+          />
           <label for="carimg">Poster:</label>
           <FileBase64
             type="file"
             multiple={false}
             max-size="1500"
-            onDone={({ base64 }) => setImgB64(base64)}
+            onDone={({ base64 }) => {
+              let padding;
+              let inBytes;
+              let base64Length;
+              if (base64.endsWith("==")) {
+                padding = 2;
+              } else if (base64.endsWith("=")) {
+                padding = 1;
+              } else {
+                padding = 0;
+              }
+
+              base64Length = base64.length;
+              console.log(base64Length);
+              inBytes = (base64Length / 4) * 3 - padding;
+              let kb = inBytes / 1000;
+              let mb = kb / 1000;
+              if (mb < 1.5) {
+                setImgB64(base64);
+              } else {
+                alert("File size too high! Size limit: 1.5MB");
+                closeModalHandler();
+              }
+            }}
           />
           <label for="carid">Car ID:</label>
           <input type="number" id="carid" name="carid" ref={idRef} />
--- src/components/CarModel/CarModel.js
+++ src/components/CarModel/CarModel.js
@@ -14,36 +14,36 @@ export default function CarModel(props) {
             <img className={`${id}-modal-img`} src={img} alt="car" />
           </div>
           <div className="car-model-details-container">
-            <div>
-              <div className="model-header">
-                <div
-                  // className="color-bar"
-                  style={{
-                    background:
-                      color.toLowerCase() !== "white" ? `${color}` : "#D4D4D4",
-                    filter: "saturate(0.7)",
-                    width: "100%",
-                    height: "60px",
-                    transform: "translateX(-500px)",
-                    WebkitAnimation: "color-bar-slide 500ms 100ms forwards",
-                    zIndex: "2",
-                    position: "relative",
-                  }}
-                >
-                  <div className="ribbon-end"></div>
-                </div>
-                <img className="logo" src={logo} alt="" />
+            <div className="model-header">
+              <div
+                // className="color-bar"
+                style={{
+                  background:
+                    color.toLowerCase() !== "white" ? `${color}` : "#D4D4D4",
+                  filter: "saturate(0.7)",
+                  width: "100%",
+                  height: "60px",
+                  transform: "translateX(-500px)",
+                  WebkitAnimation: "color-bar-slide 500ms 100ms forwards",
+                  zIndex: "2",
+                  position: "relative",
+                }}
+              >
+                <div className="ribbon-end"></div>
               </div>
-              <div>
+              <img className="logo" src={logo} alt="" />
+            </div>
+            <div className="modal-content">
+              <div className="modal-props">
                 <p>Color: {color}</p>
                 <p>Type: {docType}</p>
                 <p>Make: {make}</p>
                 <p>Model: {model}</p>
                 <p>Owner: {owner}</p>
               </div>
-            </div>
-            <div className="button-holder">
-              <button onClick={closeCarModel}>Close</button>
+              <div className="modal-button-holder">
+                <button onClick={closeCarModel}>Close</button>
+              </div>
             </div>
           </div>
         </div>
